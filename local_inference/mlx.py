459df98db4d2a0e2aaef59f2cee9fa4f8977f8d6
Loubna Ben Allal <44069155+loubnabnl@users.noreply.github.com>
2024-12-02 11:22:36 +0100

    Update mlx.py

--- local_inference/mlx.py
+++ local_inference/mlx.py
@@ -1,6 +1,13 @@
 from mlx_lm import load, generate
 
 model, tokenizer = load("HuggingFaceTB/SmolLM2-1.7B-Instruct-Q8-mlx")
-response = generate(model, tokenizer, prompt="hello", verbose=True)
 
+prompt = "Hello"
+
+messages = [{"role": "user", "content": prompt}]
+prompt = tokenizer.apply_chat_template(
+    messages, tokenize=False, add_generation_prompt=True
+)
+
+response = generate(model, tokenizer, prompt=prompt, verbose=True)
 print(response)
